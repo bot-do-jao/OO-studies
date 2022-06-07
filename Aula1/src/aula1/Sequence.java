75c4ac7a48535d4e4df7f67f4a77551ae9ca96c6
Joao Pedro <joaopedrobsb3@gmail.com>
2022-06-07 12:02:29 -0300

    test part2

--- Aula1/src/aula1/Sequence.java
+++ Aula1/src/aula1/Sequence.java
@@ -10,14 +10,15 @@ public class Sequence {
 		n1 = entry.nextDouble();
 		System.out.println("Write the second height:");
 		n2 = entry.nextDouble();
-		System.out.println("Write the third height:");
+		System.out.println("Write the third height: ");
 		n3 = entry.nextDouble();
 		
 		System.out.println("The heights are: " + n1 + ", "+ n2 + " and "+ n3);
 		
 		double sum, average;
+		int quantity = 3;
 		sum = n1 + n2 + n3;
-		average = sum/3;
+		average = sum/quantity;
 		
 		System.out.println("The average height is: " + average );
 	}
