610750f646ea287b6dde0d182af52ddc322648be
Joao Pedro <joaopedrobsb3@gmail.com>
2022-06-07 11:58:13 -0300

    first class commit since the test one didn't work out for some reason

---
+++ Aula1/src/aula1/Sequence.java
@@ -0,0 +1,24 @@
+package aula1;
+
+import java.util.Scanner;
+
+public class Sequence {
+	public static void main (String[] args) {
+		double n1, n2, n3;
+		Scanner entry = new Scanner(System.in);
+		System.out.println("Write the first height:");
+		n1 = entry.nextDouble();
+		System.out.println("Write the second height:");
+		n2 = entry.nextDouble();
+		System.out.println("Write the third height:");
+		n3 = entry.nextDouble();
+		
+		System.out.println("The heights are: " + n1 + ", "+ n2 + " and "+ n3);
+		
+		double sum, average;
+		sum = n1 + n2 + n3;
+		average = sum/3;
+		
+		System.out.println("The average height is: " + average );
+	}
+}
